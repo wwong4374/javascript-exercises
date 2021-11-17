81bbe74453c2050f54ebb8b1558fefff5486e4c8
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-16 22:26:39 -0600

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  // Base Case NaN
+  if (typeof start !== 'number' || typeof end !== 'number') {
+    return 'ERROR';
+  }
+  // Base Case < 0
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
+  // Base Case 1
+  if (start === end) {
+    return start;
+  }
 
+  // Recursive Case
+  if (start > end) {
+    // Reverse start and end
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+  return start + sumAll(start + 1, end);
 };
 
 // Do not edit below this line
