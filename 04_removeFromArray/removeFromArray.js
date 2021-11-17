66d065ab001766c0e4b3c926c359e2bbebe7b7b1
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-16 22:19:28 -0600

    Completed assignment 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...nums) {
+  // Iterate nums
+  for (var num of nums) {
+    // Find indexOf num
+    let index = array.indexOf(num);
+    if (index !== -1) {
+      // Remove from array
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
