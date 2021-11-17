0a9373da3dd89633324121540fa04a083d462225
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-16 22:40:13 -0600

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  return Math.round(((farenheit - 32) * (5 / 9)) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  return Math.round((celcius * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
