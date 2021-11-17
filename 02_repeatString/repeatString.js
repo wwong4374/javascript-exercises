f42a541f1bbbc821fa67ab02a25c5a62acdfe9f4
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-16 21:47:48 -0600

    Completed assignment 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  // Base Case < 0
+  if (num < 0) {
+    return 'ERROR';
+  }
+  // Base Case 0
+  if (num === 0) {
+    return '';
+  }
+  // Base Case 1
+  if (num === 1) {
+    return string;
+  }
 
+  // Recursive Case
+  return string.concat(repeatString(string, num - 1));
 };
 
 // Do not edit below this line
