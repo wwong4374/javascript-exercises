3f37287732f1c1ba4a1e8317c7bffa512d527d2a
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-16 21:53:28 -0600

    Completed assignment 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+  let reversed = '';
+  while(str.length > 0) {
+    reversed += str[str.length - 1];
+    str = str.slice(0, str.length - 1);
+  }
+  return reversed;
 };
 
 // Do not edit below this line
