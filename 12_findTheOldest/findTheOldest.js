c4adc40b5341a3eb85838b13df53bac00f93a73e
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-24 09:32:44 -0600

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  let oldestLiving = 0;
+  let oldestDead = 0;
+  let oldestLivingPerson = null;
+  let oldestDeadPerson = null;
 
+  people.forEach((person) => {
+    if (person.yearOfDeath) {
+      const age = person.yearOfDeath - person.yearOfBirth;
+      oldestDeadPerson = age > oldestDead ? person : oldestDeadPerson;
+      oldestDead = age > oldestDead ? age : oldestDead;
+    } else {
+      const age = currentYear - person.yearOfBirth;
+      oldestLivingPerson = age > oldestLiving ? person : oldestLivingPerson;
+      oldestLiving = age > oldestLiving ? age : oldestLiving;
+    }
+  });
+
+  return oldestLiving > oldestDead ? oldestLivingPerson : oldestDeadPerson;
 };
 
 // Do not edit below this line
