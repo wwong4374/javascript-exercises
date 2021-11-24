0032950f304a66cf197ddfbb60374998163d55fe
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-23 21:55:02 -0600

    Completed assignment 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+  numbers.forEach((num) => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  numbers.forEach((num) => {
+    product *= num;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  // BASE CASE 0
+  if (x === 0) {
+    return 1;
+  }
+  // BASE CASE 1
+  if (x === 1) {
+    return 1;
+  }
+  // Recursive Case
+  return x * factorial(x - 1);
 };
 
 // Do not edit below this line
