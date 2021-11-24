c9b02708c4dedb63d3b8b868d9e9198487785733
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-23 22:25:32 -0600

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (typeof num === 'string') {
+    let newNum = Number(num);
+    return fibonacci(newNum);
+  }
+  if (num < 0) {
+    return 'OOPS';
+  }
 
+  // BASE CASE 1
+  if (num === 1) {
+    return 1;
+  }
+  // BASE CASE 2
+  if (num === 2) {
+    return 1;
+  }
+  // RECURSIVE CASE
+  return fibonacci(num - 2) + fibonacci(num - 1);
 };
 
 // Do not edit below this line
