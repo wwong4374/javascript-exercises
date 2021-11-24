5fe3a27c53895f37843db629073a287f53ade790
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-23 22:18:07 -0600

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const punctuation = ['!', '\'', ',', '.', ' '];
+  let formattedString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!punctuation.includes(string[i])) {
+      formattedString += string[i];
+    }
+  }
+  formattedString = formattedString.toLowerCase();
+  // BASE CASE 0
+  if (formattedString.length === 0) {
+    return true;
+  }
+  // BASE CASE 1
+  if (formattedString.length === 1) {
+    return true;
+  }
+  // RECURSIVE CASE
+  if (formattedString[0] === formattedString[formattedString.length - 1]) {
+    return palindromes(formattedString.slice(1, formattedString.length - 1));
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
